4a327ab65cabea65cd3a8c1a1d52613507cb47a0
Weiqian Ni <weiqianni29@gmail.com>
2022-08-27 23:20:48 +0100

    updated background imgs, update the about page and deleted the contact page

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// const aboutContent = "Hello, my name is Weiqian Ni, and this is my personal blog website. I will update my thoughts and any interest things I have seen on this site from time to time. Hopefully in the future this blog will be full of content. If you are interested and want to know more about me, please find the details in the Contact Page:)";
 const mongoose = require("mongoose");
 const app = express();
 
@@ -26,19 +24,15 @@ const Post = mongoose.model("Post",postSchema);
 app.get("/", function(req, res){
   Post.find((err,results)=>{
     res.render("home", {
-      startingContent: homeStartingContent,
       posts: results
       });
   });
 });
 
 app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+  res.render("about");
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
 
 app.get("/compose", function(req, res){
   res.render("compose");
@@ -64,7 +58,7 @@ app.get("/posts/:postId", (req, res)=>{
     }
   });
 });
-
+// process.env.PORT
 app.listen(process.env.PORT, function() {
   console.log("Server started");
 });
