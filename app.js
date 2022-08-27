a62038f3520ca48ee497399134d5431488b2040f
Weiqian Ni <weiqianni29@gmail.com>
2022-08-27 20:25:07 +0100

    updated log

--- app.js
+++ app.js
@@ -66,5 +66,5 @@ app.get("/posts/:postId", (req, res)=>{
 });
 
 app.listen(3000 || process.env.PORT, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started");
 });
